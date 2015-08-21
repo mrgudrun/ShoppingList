57f34128c0e3543c7d2d10aef6520845c2d07d09
Runar Blekken <runar.blekken@gmail.com>
2015-08-21 18:01:57 +0200

    added missing files

---
+++ ShoppingList/ShoppingList.Web/src/app/main/main.service.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+angular.module('app.main').factory('mainService', function($http) {
+
+    var service = {};
+    service.getShoppingLists = function(userId, callback) {
+        $http.get('/webapi/api/Shoppinglist/getbyuserid/' + 1).success(function (response) {
+            callback(response);
+        });
+    }
+
+    return service;
+
+});
+
